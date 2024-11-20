b60a944720d6c42d915ada0e2becb0c29ae5341c
Clément Pasteau <4895034+ClementPasteau@users.noreply.github.com>
2024-11-20 18:25:52 +0100

    Rework Game Creation dialog

--- newIDE/app/src/MainFrame/EditorContainers/HomePage/index.js
+++ newIDE/app/src/MainFrame/EditorContainers/HomePage/index.js
@@ -119,11 +119,8 @@ type Props = {|
   canOpen: boolean,
   onChooseProject: () => void,
   onOpenRecentFile: (file: FileMetadataAndStorageProviderName) => Promise<void>,
-  onOpenExampleStore: () => void,
   onSelectExampleShortHeader: ExampleShortHeader => void,
-  onPreviewPrivateGameTemplateListingData: (
-    privateGameTemplateListingData: PrivateGameTemplateListingData
-  ) => void,
+  onSelectPrivateGameTemplateListingData: PrivateGameTemplateListingData => void,
   onOpenPrivateGameTemplateListingData: (
     privateGameTemplateListingData: PrivateGameTemplateListingData
   ) => void,
@@ -175,9 +172,8 @@ export const HomePage = React.memo<Props>(
         onChooseProject,
         onOpenRecentFile,
         onOpenNewProjectSetupDialog,
-        onOpenExampleStore,
         onSelectExampleShortHeader,
-        onPreviewPrivateGameTemplateListingData,
+        onSelectPrivateGameTemplateListingData,
         onOpenPrivateGameTemplateListingData,
         onOpenProjectManager,
         onOpenLanguageDialog,
@@ -547,10 +543,9 @@ export const HomePage = React.memo<Props>(
                       onOpenNewProjectSetupDialog={onOpenNewProjectSetupDialog}
                       onSelectExampleShortHeader={onSelectExampleShortHeader}
                       onSelectPrivateGameTemplateListingData={
-                        onPreviewPrivateGameTemplateListingData
+                        onSelectPrivateGameTemplateListingData
                       }
                       onOpenRecentFile={onOpenRecentFile}
-                      onOpenExampleStore={onOpenExampleStore}
                       onManageGame={onManageGame}
                       canManageGame={canManageGame}
                       storageProviders={storageProviders}
@@ -560,7 +555,6 @@ export const HomePage = React.memo<Props>(
                   )}
                   {activeTab === 'learn' && (
                     <LearnSection
-                      onOpenExampleStore={onOpenExampleStore}
                       onTabChange={setActiveTab}
                       selectInAppTutorial={selectInAppTutorial}
                       onOpenTemplateFromTutorial={onOpenTemplateFromTutorial}
@@ -663,10 +657,9 @@ export const renderHomePageContainer = (
     canOpen={props.canOpen}
     onChooseProject={props.onChooseProject}
     onOpenRecentFile={props.onOpenRecentFile}
-    onOpenExampleStore={props.onOpenExampleStore}
     onSelectExampleShortHeader={props.onSelectExampleShortHeader}
-    onPreviewPrivateGameTemplateListingData={
-      props.onPreviewPrivateGameTemplateListingData
+    onSelectPrivateGameTemplateListingData={
+      props.onSelectPrivateGameTemplateListingData
     }
     onOpenPrivateGameTemplateListingData={
       props.onOpenPrivateGameTemplateListingData
